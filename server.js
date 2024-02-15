b4a77ccfb66d8ab905541dce700abe2f8917feae
Vishwa <77456055+vishwa-vbn@users.noreply.github.com>
2024-02-15 12:53:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const port =  5001;
+const port =   process.env.PORT||5001;
 
 
 app.use(cors({ origin: '*' }));
